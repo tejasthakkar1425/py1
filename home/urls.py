3232fbcc7e49555d419f9f3bd54fe83a33f21d47
Nikhil Sharma <S.nikhil4@gmail.com>
2024-01-12 20:35:11 +0530

    Add css file
    Add CSS file settings into settings file.

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
 
 urlpatterns = [
     path('',views.home, name="home"),
 ]
+
+urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
