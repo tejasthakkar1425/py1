653c3bab73a3df5c48ac95f6bc2f462564e3b93f
harshthakkar148 <156444065+harshthakkar148@users.noreply.github.com>
2024-01-13 13:18:20 +0530

    Add DB Connection
    Connection with MySqlDB

--- home/urls.py
+++ home/urls.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
-from . import views
+from home.views import home
 
 urlpatterns = [
-    path('',views.home, name="home"),
+    path('',home.as_view(), name="home"),
 ]
 
 urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
