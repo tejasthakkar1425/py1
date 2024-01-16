b8a7705228e65dc05cc9db3f0801a028e88fa374
Nikhil Sharma <S.nikhil4@gmail.com>
2024-01-16 22:15:06 +0530

    Add icon in the page

--- home/urls.py
+++ home/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views
 
+app_name = "home"
+
 urlpatterns = [
     path('',views.home, name="home"),
-    path('about',views.home, name="about"),
-    path('contact',views.home, name="contact"),
-    path('service',views.home, name="service"),
-    path('signin',views.home, name="signin"),
+    path('about',views.about, name="about"),
+    path('contact',views.contact, name="contact"),
+    path('service',views.service, name="service"),
+    path('signin',views.signin, name="signin"),
 ]
--- py1/settings.py
+++ py1/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'py1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'darshan',
+        'USER': 'root',
+        'PASSWORD': 'Admin@sky12',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
