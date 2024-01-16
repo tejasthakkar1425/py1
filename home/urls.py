9584a74d01ee42de6bd5cdfd64d8a248f1fec747
tejasthakkar1425 <156438920+tejasthakkar1425@users.noreply.github.com>
2024-01-16 16:53:11 +0530

    urls.py

--- home/urls.py
+++ home/urls.py
@@ -6,6 +6,10 @@ from home.views import home
 
 urlpatterns = [
     path('',home.as_view(), name="home"),
+    path('about',views.home, name="about"),
+    path('contact',views.home, name="contact"),
+    path('service',views.home, name="service"),
+    path('signin',views.home, name="signin"),
 ]
 
 urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
